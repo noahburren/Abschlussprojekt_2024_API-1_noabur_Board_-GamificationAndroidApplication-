7bf243bc736bab5ac6b25b269cac34975ecfbc40
noahburren <noah.burren@bluewin.ch>
2024-06-04 10:30:03 +0200

    App Routes signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Login from "./Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
