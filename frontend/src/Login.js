77ad3777d866365ed0633faf8c49ea28742865b1
noahburren <noah.burren@bluewin.ch>
2024-06-04 11:00:20 +0200

    Handle input

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./LoginValidation";
 
 function Login() {
   const [values, setValues] = useState({
     email: "",
     password: "",
   });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign-in</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email</strong>
@@ -18,6 +34,8 @@ function Login() {
             <input
               type="email"
               placeholder="Enter Email"
+              name="email"
+              onChange={handleInput}
               className="form-control rounded-0"
             />
           </div>
@@ -28,10 +46,14 @@ function Login() {
             <input
               type="password"
               placeholder="Enter Password"
+              name="password"
+              onChange={handleInput}
               className="form-control rounded-0"
             />
           </div>
-          <button className="btn btn-success w-100 rounded-0">Log in</button>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Log in
+          </button>
           <p></p>
           <Link
             to="/signup"
