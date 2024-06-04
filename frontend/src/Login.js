6d5dd525dcd7a27f13df452e23c138df845bfecf
noahburren <noah.burren@bluewin.ch>
2024-06-04 10:20:16 +0200

    Email und passwort feld zentrieren

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -7,11 +7,19 @@ function Login() {
         <form action="">
           <div className="mb-3">
             <label htmlFor="email">Email</label>
-            <input type="email" placeholder="Enter Email" />
+            <input
+              type="email"
+              placeholder="Enter Email"
+              className="form-control rounded-0"
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Enter Password" />
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="form-control rounded-0"
+            />
           </div>
           <button className="btn btn-success">Log in</button>
           <p></p>
