cf491771c00780162928e6c71dd511df731b91d4
noahburren <noah.burren@bluewin.ch>
2024-06-04 10:16:52 +0200

    Login form

---
+++ frontend/src/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Enter Email" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Enter Password" />
+          </div>
+          <button className="btn btn-success">Log in</button>
+          <p></p>
+          <button className="btn btn-default border">Create Account</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
