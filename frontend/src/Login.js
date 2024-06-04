7f4c3569d20188b6fea44228ff953baf792b819d
noahburren <noah.burren@bluewin.ch>
2024-06-04 10:43:53 +0200

    h2 titel

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,6 +5,7 @@ function Login() {
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
+        <h2>Sign-in</h2>
         <form action="">
           <div className="mb-3">
             <label htmlFor="email">
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign-Up</h2>
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button className="btn btn-success w-100 rounded-0">Sign up</button>
+          <p></p>
+          <Link
+            to="/"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Log in
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
