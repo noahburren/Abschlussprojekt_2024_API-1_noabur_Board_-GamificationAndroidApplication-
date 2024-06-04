4fdc1f0ff9bce5ee400569f1ab87cb2c102f3354
noahburren <noah.burren@bluewin.ch>
2024-06-04 10:34:03 +0200

    signup import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
 
 function App() {
   return (
