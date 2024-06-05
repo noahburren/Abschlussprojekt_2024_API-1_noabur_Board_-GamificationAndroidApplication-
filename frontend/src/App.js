ccb417f9c8347cba31f825d9ecc24107b72c66d8
noahburren <noah.burren@bluewin.ch>
2024-06-05 08:23:02 +0200

    restart wegen fehler am schluss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
-import Login from "./Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./Signup";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
