206fce26d6c87390e007e7ad402ee3af704be3e8
noahburren <noah.burren@bluewin.ch>
2024-06-05 10:25:34 +0200

    alles nochmals gemacht

---
+++ frontend/src/Login.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Validation from "./LoginValidation";
+
+function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const handeInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(Validation(values));
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign-In</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              onChange={handeInput}
+              className="form-control rounded-0"
+            />
+            {errors.email && (
+              <span className="text-danger"> {errors.email}</span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              onChange={handeInput}
+              className="form-control rounded-0"
+            />
+            {errors.password && (
+              <span className="text-danger"> {errors.password}</span>
+            )}
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Log in
+          </button>
+          <p>You are agree to our terms and policies</p>
+          <Link
+            to="/signup"
+            className="btn btn-default w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Create Account
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
