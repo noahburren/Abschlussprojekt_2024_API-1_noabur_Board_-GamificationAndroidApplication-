cc4d6942e56fc702e3dec51a05f956748ea064f9
noahburren <noah.burren@bluewin.ch>
2024-06-17 13:25:08 +0200

    exercises pfad hinzufügen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import Login from "./Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
 import Signup from "./Signup";
 import Home from "./Home";
+import Exercises from "./Exercises";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route element={<ProtectedRoutes />}>
-          <Route path="/home" element={<Home />}></Route>
+          <Route path="/home" element={<Home />} />
+          <Route path="/exercises/:category" element={<Exercises />} />
         </Route>
       </Routes>
     </BrowserRouter>
