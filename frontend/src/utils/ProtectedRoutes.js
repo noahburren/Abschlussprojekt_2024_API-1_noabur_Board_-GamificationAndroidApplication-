403862c706f8f90e6849cef305a24b1778b61179
noahburren <noah.burren@bluewin.ch>
2024-06-19 08:56:02 +0200

    protected route

--- frontend/src/utils/ProtectedRoutes.js
+++ frontend/src/utils/ProtectedRoutes.js
@@ -1,8 +1,14 @@
+import React, { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import { AuthContext } from "../AuthContext"; // Pfade entsprechend anpassen
 
 const ProtectedRoutes = () => {
-  const user = true;
-  return user ? <Outlet /> : <Navigate to="/" />;
+  const { userId } = useContext(AuthContext); // AuthContext verwenden, um userId zu erhalten
+
+  // Überprüfen, ob userId vorhanden ist
+  const isAuthenticated = !!userId; // Wandelt userId in ein Boolean um (true, wenn userId vorhanden ist, sonst false)
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoutes;
