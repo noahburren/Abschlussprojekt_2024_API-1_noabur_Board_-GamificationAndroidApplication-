57ba760169b86a07124dda46d4de43f815f3028e
noahburren <noah.burren@bluewin.ch>
2024-06-19 16:38:56 +0200

    fehlermeldung passwort

--- frontend/src/LoginValidation.js
+++ frontend/src/LoginValidation.js
@@ -1,32 +1,21 @@
 function Validation(values) {
-  let errors = {}; // Initialisiert ein leeres Objekt für Fehlermeldungen
-
-  // Definiert Regular Expressions für E-Mail und Passwort
+  let errors = {};
   const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s]+$/;
   const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
 
-  // Validierung der E-Mail
   if (!values.email) {
     errors.email = "Email darf nicht leer sein";
   } else if (!email_pattern.test(values.email)) {
     errors.email = "Ungültige Email";
   }
 
-  // Validierung des Passworts
   if (!values.password) {
-    errors.password = "Passwort darf nicht leer sein";
-  } else if (values.password.length < 8) {
-    errors.password = "Passwort muss mindestens 8 Zeichen lang sein";
-  } else if (!/\d/.test(values.password)) {
-    errors.password = "Passwort muss mindestens eine Zahl enthalten";
-  } else if (!/[A-Z]/.test(values.password)) {
-    errors.password =
-      "Passwort muss mindestens einen Grossbuchstaben enthalten";
+    errors.password = "Password darf nicht leer sein";
   } else if (!password_pattern.test(values.password)) {
-    errors.password = "Passwort entspricht nicht den Vorgaben";
+    errors.password = "Passwort ungültig";
   }
 
-  return errors; // Gibt das Objekt mit den Validierungsfehlern zurück
+  return errors;
 }
 
-export default Validation; // Exportiert die Validierungsfunktion
+export default Validation;
--- frontend/src/SignupValidation.js
+++ frontend/src/SignupValidation.js
@@ -24,6 +24,12 @@ function Validation(values) {
   // Validierung des Passworts
   if (values.password === "") {
     errors.password = "Passwort darf nicht leer sein";
+  } else if (values.password.length < 8) {
+    errors.password = "Passwort muss mindestens 8 Zeichen lang sein";
+  } else if (!/\d/.test(values.password)) {
+    errors.password = "Passwort muss mindestens eine Zahl enthalten";
+  } else if (!/[A-Z]/.test(values.password)) {
+    errors.password = "Passwort muss mindestens einen Großbuchstaben enthalten";
   } else if (!password_pattern.test(values.password)) {
     errors.password = "Ungültiges Passwort";
   } else {
